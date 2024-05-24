35ab59f749eb0a397c3a8c6dad7516004764d207
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-24 08:39:29 +0300

    Reworked test_not_none.py to accurately test the behavior of the return_not_none function

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -7,5 +7,5 @@ from not_none_functions import return_not_none
 #     assert False
 
 def test_return_not_none():
-    '''in not_none_functions, function "return_not_none" returns a value that is None.'''
-    assert True
+    '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
+    assert return_not_none() == None
